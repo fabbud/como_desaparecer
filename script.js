252c5dce38738a96cc99be56ffc19cd184bf96fa
fabbud <fabbud@gmail.com>
2020-10-30 15:47:52 +0000

    homepage e footer

---
+++ script.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', './assets/instagram_amarelo.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', './assets/instagram_branco.png');
+  }
